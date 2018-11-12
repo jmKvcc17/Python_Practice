e73423c6b62838af54c04d0687ea6feafb396a38
jmKvcc17 <jmeachum17@gmail.com>
2018-11-12 14:27:41 -0500

    Working

--- arrThread.py
+++ arrThread.py
@@ -1,17 +1,29 @@
 import threading
-import time
+from threading import Lock
+
+
+# Globals
+# create the lock object
+lock = Lock()
+
 
 def swap(arr, second_half_bool):
     
-    if second_half_bool == False:
+    if not second_half_bool:
+        lock.acquire()
         arr[0:5] = arr[0:5][::-1]
+        lock.release()
     else:
+        lock.acquire()
         arr[5:10] = arr[5:10][::-1]
+        lock.release()
 
 
 if __name__ == '__main__':
 
     arr = list(range(1, 11))
+
+    actArr = arr.array('i', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
     
     try:
        t1 = threading.Thread(target=swap, args=(arr, False))
@@ -20,10 +32,8 @@ if __name__ == '__main__':
        t1.start()
        t2.start()
     except:
-        print("An error has occured in a thread")
+        print("An error has occurred in a thread")
 
-    
-    
     for i in arr:
         print(i)
 
